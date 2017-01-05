56360d72d28b24b63057ae2fc2c127a989c80be7
MasterHo1228 <masterho1228@gmail.com>
2017-01-05 16:39:36 +0800

    Initial backend route list

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,33 @@
 Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 
+//Articles
+Route::resource('articles', 'ArticlesController', ['only' => ['show']]);
+
+//前台
+Route::group(['middleware' => 'guest'],function() {
+    Route::get('signin','SessionsController@create')->name('signin');
+    Route::post('signin','SessionsController@store')->name('signin');
+});
+
+//后台
+Route::group(['prefix' => 'backyard'],function (){
+    Route::group(['middleware' => 'auth'],function() {
+        Route::any('/','backend\HomeController@index');
+        Route::resource('home', 'Backend\HomeController');
+        Route::resource('article', 'Backend\ArticlesController');
+    });
+});
+Route::group(['middleware' => 'guest'],function() {
+    Route::get('login', 'Auth\LoginController@showLoginForm');
+    Route::post('login', 'Auth\LoginController@login');
+});
+
+
 //Users
-Route::get('/signup', 'UsersController@create')->name('signup');
+//Route::get('/signup', 'UsersController@create')->name('signup');
 //Route::resource('users', 'UsersController');
 //Activation
-Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
+//Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 
-//Sessions
-//Route::get('signin','SessionsController@create')->name('signin');
-//Route::post('signin','SessionsController@store')->name('signin');
 //Route::delete('signout', 'SessionsController@destroy')->name('signout');
-
-//Articles
-Route::resource('articles', 'ArticlesController', ['only' => ['create', 'show', 'store', 'edit', 'update', 'destroy']]);
-
-//backend
-Route::group(['prefix' => 'backyard', 'middleware' => 'auth'], function () {
-    Route::resource('home', 'Backend\HomeController');
-    Route::resource('article', 'Backend\ArticlesController');
-});
