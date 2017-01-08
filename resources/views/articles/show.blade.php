8d44b7937c40441ab3a2b49c8225584ded34ef72
MasterHo1228 <masterho1228@gmail.com>
2017-01-08 16:44:05 +0800

    Home article detail page -- Display article created date and updated date.

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -7,7 +7,11 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <h1 class="title text-center">{{ $article->title }}</h1>
-                    <h6 class="text-center">阅读次数:{{ $article->read_count }}</h6>
+                    <h6 class="text-center">
+                        发布日期:{{ $article->created_at }}&nbsp;&nbsp;&nbsp;
+                        <b>阅读次数:{{ $article->read_count }}</b>&nbsp;&nbsp;&nbsp;
+                        <b>更新时间:{{ $article->updated_at }}</b>
+                    </h6>
                     <p class="description">{!! $article_content !!}</p>
                 </div>
             </div>
