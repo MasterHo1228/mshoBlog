993a1ee210819f2d6cecb6bfd20e52f1b8be3947
MasterHo1228 <masterho1228@gmail.com>
2017-01-13 16:32:50 +0800

    Home -- Update about page.

--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -7,7 +7,13 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
                     <h2 class="title">关于</h2>
-                    <h5 class="description">mshoBlog</h5>
+                    <p class="description">
+                        <b>mshoBlog</b>
+                        <small>Version:0.1 alpha</small>
+                        <br/><br/>
+                        该博客网站基于Laravel 5.3开发<br/>
+                        网站GitHub开源地址:<a href="https://github.com/MasterHo1228/mshoBlog" target="_blank">https://github.com/MasterHo1228/mshoBlog</a><br/>
+                    </p>
                 </div>
             </div>
         </div>
