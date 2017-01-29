1220a97718334dd3e18df23a51d179b01bd46324
MasterHo1228 <masterho1228@gmail.com>
2017-01-29 10:29:46 +0800

    Try to fix js bug

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -23,14 +23,3 @@ $(document).ready(function() {
     $.material.init();
     $(".wechat_link").tooltip({html: true});
 });
-
-function flashy(message, link) {
-    var template = $($("#flashy-template").html());
-    $(".flashy").remove();
-    template.find(".flashy__body").html(message).attr("href", link || "#").end()
-        .appendTo("body").hide().fadeIn(300).delay(2800).animate({
-        marginRight: "-100%"
-    }, 300, "swing", function () {
-        $(this).remove();
-    });
-}
