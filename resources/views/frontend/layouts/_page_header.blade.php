07967d1359750fb4ae84b58ab03dcb4d128c0c32
MasterHo1228 <masterho1228@gmail.com>
2017-02-03 22:49:22 +0800

    Frontend -- Fixed duplicate footer bug in sign up page

--- resources/views/frontend/layouts/_page_header.blade.php
+++ resources/views/frontend/layouts/_page_header.blade.php
@@ -4,9 +4,7 @@
         @include('frontend.static_pages._index_header')
     @elseif(Request::is('signup'))
         @include('frontend.users._signup')
-        @include('frontend.layouts._footer')
     @elseif(Request::is('signin'))
         @include('frontend.sessions._signin')
-        @include('frontend.layouts._footer')
     @endif
 </div>
